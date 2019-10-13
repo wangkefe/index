b09acc9b19d8ea5f3ba08b84a58761bfb750cf59
wangkefe <1253986353@qq.com>
2019-10-13 12:40:23 +0800

    第二次提交

---
+++ myweb/src/components/myweb.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class='imooc'>
+    <header class="header">
+      <ul class="uList">
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  header.header{
+    width:100%;
+    height:50px;
+    background-color:#36B708;
+  }
+</style>
--- myweb/src/router/index.js
+++ myweb/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import myweb from '@/components/myweb'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path: '/myweb',
+      name: 'myweb',
+      component: myweb
     }
   ]
 })
